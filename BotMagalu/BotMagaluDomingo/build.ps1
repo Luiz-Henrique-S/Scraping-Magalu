d04cf0e5c9c9cb031280c89e9f6b2333e435c51f
Luiz-Henrique-S <luiz2562@gmail.com>
2024-04-22 22:39:21 -0300

    first commit

---
+++ BotMagalu/BotMagaluDomingo/build.ps1
@@ -0,0 +1,3 @@
+$exclude = @("venv", "BotMagaluDomingo.zip")
+$files = Get-ChildItem -Path . -Exclude $exclude
+Compress-Archive -Path $files -DestinationPath "BotMagaluDomingo.zip" -Force
---
+++ BotMagalu/BotMagaluDomingo/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+zip -r "BotMagaluDomingo.zip" * -x "BotMagaluDomingo.zip"
